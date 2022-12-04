ba3bc471d6cf9174eadaeeee388b41724a5cf980
goldipl <mgodlewski92@gmail.com>
2022-12-04 21:49:50 +0100

    add dist

--- src/components/article/ArticleStyle.jsx
+++
@@ -1,8 +0,0 @@
-import styled from "styled-components";
-
-export const ArticleContainer = styled.div `
-    margin: 0 auto;
-    height: 60vh;
-    background-color: #e5e4e2;
-    padding: 64px 16vw 64px 16vw;
-`;
